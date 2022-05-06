24c494fe8836e0112bfd039c1e6f221b9c10f17f
shubhampatil5128 <31090767+shubhampatil5128@users.noreply.github.com>
2022-05-06 08:11:23 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -29,8 +29,6 @@ resource "aws_eks_cluster" "eks" {
   vpc_config {
     subnet_ids = [aws_subnet.public1.id, aws_subnet.public2.id, aws_subnet.private1.id, aws_subnet.private2.id]
   }
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy-AmazonEKSClusterPolicy
   ]
